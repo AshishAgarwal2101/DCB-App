029676e097c40fac4b4f6cd5ec39709e29cba00e
VarshaJambunathan <jvarsha621@gmail.com>
2017-11-24 21:27:01 +0530

    Navigation Drawer for accounts(two) added;

--- app/src/main/java/com/cashless/easycash/MainActivity.java
+++ app/src/main/java/com/cashless/easycash/MainActivity.java
@@ -1,20 +1,47 @@
 package com.cashless.easycash;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     ImageView bSend, bReceive, bTransaction, bInvest, bFAQs, bAddNewNumber;
     Intent i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Easy Cash");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_account_1);
+        Toast.makeText(this, "Using Account 1: xxxx xxxx xxxx xxxx", Toast.LENGTH_SHORT).show();
 
         bSend = (ImageView) findViewById(R.id.icon_send);
         bReceive = (ImageView) findViewById(R.id.icon_receive);
@@ -24,6 +51,7 @@ public class MainActivity extends AppCompatActivity{
         bAddNewNumber=(ImageView)findViewById(R.id.icon_num);
         i= new Intent(this, ChoosePhoneActivity.class);
         i.putExtra("new","newNumber");
+
 
         bSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,5 +89,32 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_account_1) {
+            Toast.makeText(this, "Using Account 1: xxxx xxxx xxxx xxxx", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_account_2) {
+            Toast.makeText(this, "Using Account 2: xxxx xxxx xxxx xxxx", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
