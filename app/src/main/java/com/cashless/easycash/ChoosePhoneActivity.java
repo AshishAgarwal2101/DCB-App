9c74dbbaaae0c4999b3758e672c4866d5c96c784
ApoorvaJaiswal <jais.appy19960407@gmail.com>
2017-12-11 21:41:45 +0530

    Merge branch 'master' of https://github.com/AshishAgarwal2101/DCB-App
    # Conflicts:
    #	app/src/main/java/com/cashless/easycash/ChoosePhoneActivity.java
    #	cognalys/build/generated/mockable-android-19.v3.jar
    #	cognalys/build/generated/source/aidl/debug/com/android/internal/telephony/ITelephony.java
    #	cognalys/build/intermediates/blame/res/androidTest/debug/multi-v2/debug.json
    #	cognalys/build/intermediates/blame/res/androidTest/debug/multi-v2/values-w820dp-v13.json
    #	cognalys/build/intermediates/blame/res/androidTest/debug/single/debug.json
    #	cognalys/build/intermediates/incremental/compileDebugAidl/dependency.store
    #	cognalys/build/intermediates/incremental/debug-mergeJniLibs/merge-state
    #	cognalys/build/intermediates/incremental/mergeDebugAndroidTestResources/compile-file-map.properties
    #	cognalys/build/intermediates/incremental/mergeDebugAndroidTestResources/merger.xml
    #	cognalys/build/intermediates/incremental/mergeDebugAssets/merger.xml
    #	cognalys/build/intermediates/incremental/mergeDebugJniLibFolders/merger.xml
    #	cognalys/build/intermediates/incremental/mergeDebugShaders/merger.xml
    #	cognalys/build/intermediates/incremental/packageDebugResources/compile-file-map.properties
    #	cognalys/build/intermediates/incremental/packageDebugResources/merger.xml
    #	cognalys/build/intermediates/res/merged/androidTest/debug/anim_rotate.xml.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-hdpi-v4_ic_launcher.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-hdpi-v4_loading.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-ldpi-v4_loading.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-mdpi-v4_ic_launcher.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-mdpi-v4_loading.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-xhdpi-v4_ic_launcher.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-xhdpi-v4_loading.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-xxhdpi-v4_ic_launcher.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/drawable-xxhdpi-v4_loading.png.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/layout_activity_main.xml.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/layout_activity_verify_mobile.xml.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/menu_main.xml.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/menu_verify_mobile.xml.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/values-w820dp-v13_values-w820dp-v13.arsc.flat
    #	cognalys/build/intermediates/res/merged/androidTest/debug/values_values.arsc.flat
    #	cognalys/build/intermediates/transforms/mergeJniLibs/debug/__content__.json
    #	cognalys/build/outputs/logs/manifest-merger-debug-report.txt

--- app/src/main/java/com/cashless/easycash/ChoosePhoneActivity.java
+++ app/src/main/java/com/cashless/easycash/ChoosePhoneActivity.java
@@ -1,11 +1,21 @@
 
 package com.cashless.easycash;
 
+import android.*;
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,11 +27,14 @@ import android.widget.Toast;
 
 import com.cashless.easycash.Helpers.SPHelper;
 
+import java.security.Permission;
+
 /**
  * Created by Ashish on 11/4/2017.
  */
 
-public class ChoosePhoneActivity  extends AppCompatActivity{
+public class
+ChoosePhoneActivity  extends AppCompatActivity{
 
     TextView t1,t2;
     RadioButton r1;
@@ -30,17 +43,21 @@ public class ChoosePhoneActivity  extends AppCompatActivity{
     String s1="Jio 4G";
     Button b;
     Intent i;
+    Boolean permissionsGranted=false;
     String phn="8888877777",bankName="DCB",vpa="534534@ybl",accno="31241441414",name="Varsha",
-            id,branch="Jayanagar",ifsc="ubi00000127",vpaPin="1234",appPin="2345";
-    int k=0;
+            id,branch="Jayanagar",ifsc="ubi00000127",vpaPin="1234",appPin="2345",number = null;
+    int k=0,currentAccount=0;
     Intent newNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_phone_layout);
+        callLogPermissionDialog();
         newNum= new Intent();
         newNum= getIntent();
+        loadAccountBeingUsed();
+        k=currentAccount;
         load();
         if(newNum.hasExtra("new"))
         {
@@ -61,22 +78,194 @@ public class ChoosePhoneActivity  extends AppCompatActivity{
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String number= e.getText().toString();
-                i.putExtra("number",number);
-                startActivity(i);
+                number= e.getText().toString();
+                if(!number.equals("") && permissionsGranted) {
+                    if(!((number.substring(0,3)).equals("+91")))
+                        number = "+91" + number ;
+                    if(number.length() != 13)
+                        Toast.makeText(getApplicationContext(),"Enter the correct number",Toast.LENGTH_SHORT).show();
+                    else {
+                        Toast.makeText(getApplicationContext(), number, Toast.LENGTH_SHORT).show();
+                        i.putExtra("number", number);
+                        startActivity(i);
+                    }
+                }
+                else if(!permissionsGranted)
+                {
+                    Toast.makeText(getApplicationContext(),"Provide permissions to continue!",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Enter the number",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
+    void callLogPermissionDialog()
+    {
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.READ_CALL_LOG)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CALL_LOG)) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Call Log access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("please confirm Call Log access");//TODO put real question
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.READ_CALL_LOG}
+                                    , 0);
+                        }
+                    });
+                    builder.show();
+
+                } else {
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_CALL_LOG},
+                            0);
+                }
+            }
+            else//permission granted
+            {
+                locationPermissionDialog();
+            }
+        }
+    }
+    void locationPermissionDialog()
+    {
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Location access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please confirm Location access");//TODO put real question
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.ACCESS_FINE_LOCATION}
+                                    , 1);
+                        }
+                    });
+                    builder.show();
+
+                } else {
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            1);
+                }
+            }
+            else
+            {
+                contactsPermissionDialog();
+            }
+        }
+    }
+    void contactsPermissionDialog()
+    {
+
+        if(Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.READ_CONTACTS)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Location access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please confirm Location access");//TODO put real question
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.READ_CONTACTS}
+                                    , 2);
+                        }
+                    });
+                    builder.show();
+
+                } else {
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            2);
+                }
+            }
+            else//permission granted
+            {
+               permissionsGranted = true;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode==0) {
+            if ((grantResults[0] == PackageManager.PERMISSION_GRANTED))//call logs
+            {
+                locationPermissionDialog();
+            } else {
+                Toast.makeText(getApplicationContext(), "Call logs permission needs to be granted to proceed!", Toast.LENGTH_SHORT).show();
+            }
+        }
+           else if(requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)//location
+            {
+                contactsPermissionDialog();
+            } else {
+                Toast.makeText(getApplicationContext(), "Contacts permission needs to be granted to proceed!", Toast.LENGTH_SHORT).show();
+            }
+        }
+            else if(requestCode == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)//location
+            {
+                permissionsGranted = true;
+            } else {
+                Toast.makeText(getApplicationContext(), "Location permission needs to be granted to proceed!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
     public void load() {
-        phn = SPHelper.getSP(getApplicationContext(),"phone"+k, phn);
-        bankName = SPHelper.getSP(getApplicationContext(),"bankName"+k, bankName);
-        vpa = SPHelper.getSP(getApplicationContext(),"vpa"+k, vpa);
-        accno = SPHelper.getSP(getApplicationContext(),"accno"+k,accno);
-        name = SPHelper.getSP(getApplicationContext(),"name"+k, name);
-        branch = SPHelper.getSP(getApplicationContext(),"branch"+k,branch);
-        ifsc = SPHelper.getSP(getApplicationContext(),"ifsc"+k, ifsc);
-        vpaPin = SPHelper.getSP(getApplicationContext(),"vpapin"+k,vpaPin);
-        appPin = SPHelper.getSP(getApplicationContext(),"apppin"+k,appPin);
+        phn = SPHelper.getSP(getApplicationContext(),"phone"+k, "8888877777");
+        bankName = SPHelper.getSP(getApplicationContext(),"bankName"+k, "DCB");
+        vpa = SPHelper.getSP(getApplicationContext(),"vpa"+k, "534534@ybl");
+        accno = SPHelper.getSP(getApplicationContext(),"accno"+k,"31241441414");
+        name = SPHelper.getSP(getApplicationContext(),"name"+k, "Ronit");
+        branch = SPHelper.getSP(getApplicationContext(),"branch"+k,"Jayanagar");
+        ifsc = SPHelper.getSP(getApplicationContext(),"ifsc"+k, "ubi00000127+");
+        vpaPin = SPHelper.getSP(getApplicationContext(),"vpapin"+k,"1234");
+        appPin = SPHelper.getSP(getApplicationContext(),"apppin"+k,"2345");
+        //++k;
+    }
+    public void loadAccountBeingUsed(){
+        currentAccount = SPHelper.getSP1(getApplicationContext(),"currentAccount",currentAccount);
     }
 }
